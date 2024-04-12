976b184682e8e74bb33648cf4421f7afa07010b0
Nguyen Tuan Anh <21120410@student.hcmus.edu.vn>
2024-04-12 18:42:54 +0700

    Moving classes and functions from Main to Frame

--- Main.py
+++ Main.py
@@ -30,54 +30,6 @@ def Function_Search(g: Grid, sc: pygame.Surface):
     show_cost(cost, sc)
 
 
-# Tìm đường đi mới
-def find_path(queue_request, queue_response, space: Grid, sc):
-    while True:
-        # Đợi yêu cầu tìm đường
-        _ = queue_request.get()
-
-        path = []
-        #path = AStar(space, space.Start, space.Goal)
-        path = find_path_with_pickup_points_using_matching(space, space.Start, space.Goal, space.pickup_points)
-
-        # Trả về đường đi
-        queue_response.put(path)
-
-class Agent:
-    def __init__(self, space: Grid, sc: pygame.Surface, frame: Frame, path, queue_request: Queue, queue_response: Queue):
-        self.count = 0
-        self.delay = 0
-        self.is_arrived = False
-        self.space = space
-        self.sc = sc
-        self.frame = frame
-        self.path = path
-        self.queue_request = queue_request
-        self.queue_response = queue_response
-
-    def update(self):
-        if self.is_arrived is not True:
-            self.count += 1
-            if self.count % 2 == 0:
-                self.frame.update_new_frame()
-                pygame.display.flip()
-            if self.delay == 0:
-                result = self.frame.update_agent(self.path)
-                if result == False:
-                    self.queue_request.put(1)
-                    self.path = []
-                if result == "delay":
-                    self.delay = 5
-            else:
-                self.delay -= 1
-            pygame.time.delay(100)
-            if not self.queue_response.empty():
-                self.path = self.queue_response.get()
-                draw_path(self.space, self.sc, self.path, YELLOW)
-                pygame.display.flip()
-                clear_path(self.space, self.sc, self.path)
-            pygame.display.flip()
-
 def main_level_4(data):
     pygame.init()
     pygame.display.set_caption(f'project1')
@@ -95,7 +47,7 @@ def main_level_4(data):
     queue_response = Queue()
 
     # Đa nhiệm tìm đường
-    thread0 = threading.Thread(target=find_path, args=(queue_request, queue_response, space, sc))
+    thread0 = threading.Thread(target=find_new_path, args=(queue_request, queue_response, space, sc))
     thread0.start()
 
     # Tìm đường đi khởi đầu
